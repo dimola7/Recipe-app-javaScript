3c012d7c2d2f36de6024729cdcded50c693e5049
Dimola Ogidan <didiogidan@gmail.com>
2020-06-22 14:03:43 +0100

    added ingredient list

--- recipeDetails.js
+++ recipeDetails.js
@@ -1,6 +1,6 @@
 const details = document.querySelector(".recipe-details");
 
-const recipeDetails = (data) => {
+const renderUI = (data) => {
   const recipe = data;
   details.innerHTML = `
   <div class="picture-container">
@@ -14,15 +14,18 @@ const recipeDetails = (data) => {
         <h1>${recipe.title}</h1>
         <h3>By <a href="${recipe.source_url}">${recipe.publisher}</a></h3>
         <h3 class="ingredients-head">Ingredients</h3>
-        <ul>
-            <li>${recipe.ingredients}</li>
-        </ul>
+        <ul class="ingredients-list"></ul>
         <div>
           <a class="button" href="${recipe.source_url}">See more</a>
         </div>
     </div>     
           `;
-};
+  const renderList = details.querySelector('.ingredients-list')
+  recipe.ingredients.forEach((ingredient) => {
+    const list = `<li>${ingredient}</li>`;
+    renderList.innerHTML += list;
+  });
+}
 
 addEventListener("DOMContentLoaded", (e) => {
   const search = new URLSearchParams(window.location.search);
@@ -30,7 +33,7 @@ addEventListener("DOMContentLoaded", (e) => {
   const id = search.get("rId");
   if (id) {
     recipeDets(id)
-      .then((data) => recipeDetails(data))
+      .then((data) => renderUI(data))
       .catch((err) => console.log(err));
   }
 });
